75e761ab3f49e9d7d22f339133387a687891584c
Filipe Gomes <filipe.gt1998@outlook.com>
2022-04-19 07:58:30 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import React from 'react'
+
+const Home = () => {
+
+
+  return (
+    <>
+      <Head>
+        <title>Início | Ignews</title>
+      </Head>
+      <div>Home</div>
+    </>
+  )
+}
+
+export default Home
