66f576f7fb3b19ccc439e9170825652e7f187724
Filipe Gomes <filipe.gt1998@outlook.com>
2022-04-25 07:43:51 -0300

    This commit adds SSG and SSR in next.js

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import style from "./styles.module.scss";
 
-const SubscribeButton = () => {
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
+const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
   return (
     <button type="button" className={style.subscribeButton}>
       Subscribe now
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Head from "next/head";
-import styles from "./home.module.scss";
 import SubscribeButton from "../components/SubscribeButton";
+import { GetStaticProps } from "next";
+import styles from "./home.module.scss";
+import { stripe } from "../services/stripe";
 
-const Home = () => {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+const Home = ({ product }: HomeProps) => {
   return (
     <>
       <Head>
@@ -17,9 +26,9 @@ const Home = () => {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for ${product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
@@ -28,3 +37,23 @@ const Home = () => {
 };
 
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1KsOShDNBM8AHCCyEYHQH7de");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+
+    revalidate: 60 * 60 * 24, // 1 day
+  };
+};
